56dab9ab2172730c4ae8501304b7a56bef0db466
peng45588 <913704951@qq.com>
2015-12-12 19:36:28 +0800

    update:    我 界面功能完善:    (1)密码修改方面,点击设置显示密码新密码三框(建议把帐号一栏弄回来)    (2)添加scroll属性,可以上下滑动以完全显示,将退出帐号弄回来了(小明若没有把退出放到哪里的想法暂时先留在这里)
       主界面:
       (1)将右上角的...改成+,由于目前+里面只包含一个[发布图片],所以一点击+就直接进入发布界面
       (2)发布图片界面焦点改变,不再弹出输入框

--- app/src/main/java/com/zzt/circle/app/fragment/MyInfoFragment.java
+++ app/src/main/java/com/zzt/circle/app/fragment/MyInfoFragment.java
@@ -1,18 +1,21 @@
 package com.zzt.circle.app.fragment;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.*;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.zzt.circle.app.Config;
 import com.zzt.circle.app.R;
+import com.zzt.circle.app.activity.LoginActivity;
+import com.zzt.circle.app.net.UpdateInfo;
+import com.zzt.circle.app.tools.ImageUtil;
+import com.zzt.circle.app.tools.MD5Utils;
 
 /**
  *
@@ -29,11 +32,18 @@ public class MyInfoFragment extends LazyFragment {
 
     private ImageLoader imageLoader;
 
-    private EditText tv_gender;
-    private EditText tv_nickname;
+    private TextView tv_pw_old;
+    private TextView tv_pw_new;
+    private TextView tv_pw_new2;
+    private EditText et_pw;
+    private EditText et_pw2;
+    private EditText et_pw_old;
+    private EditText et_gender;
+    private EditText et_nickname;
     private ImageView iv_image;
 
     private Button setInfo;
+    private Button delsetInfo;
 
     public MyInfoFragment() {
         // Required empty public constructor
@@ -48,92 +58,126 @@ public class MyInfoFragment extends LazyFragment {
         gender = Config.getCachedGender(getActivity());
         nickname = Config.getCachedNickname(getActivity());
         avatarUrl = Config.getCachedAvatagUrl(getActivity());
-        View rootView = inflater.inflate(R.layout.fragment_myinfo, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_myinfo, container, false);
         isPrepared = true;
 
-        //fuzhi
-        tv_gender = (EditText) rootView.findViewById(R.id.gender);
-        tv_nickname = (EditText) rootView.findViewById(R.id.nickname);
+        // 赋值
+        et_gender = (EditText) rootView.findViewById(R.id.gender);
+        et_nickname = (EditText) rootView.findViewById(R.id.nickname);
         iv_image = (ImageView) rootView.findViewById(R.id.iv_image);
 
-        tv_gender.setEnabled(false);
-        tv_nickname.setEnabled(false);
+        et_gender.setEnabled(false);
+        et_nickname.setEnabled(false);
 
         imageLoader = ImageLoader.getInstance();
         imageLoader.displayImage(Config.SERVER_URL + avatarUrl, iv_image);
-        tv_nickname.setText(nickname);
-        tv_gender.setText(gender);
+        et_nickname.setText(nickname);
+        et_gender.setText(gender);
+
+        tv_pw_new = (TextView) rootView.findViewById(R.id.tv_pw_new);
+        tv_pw_new2 = (TextView) rootView.findViewById(R.id.tv_pw_new2);
+        tv_pw_old = (TextView) rootView.findViewById(R.id.tv_pw_old);
+        et_pw = (EditText) rootView.findViewById(R.id.et_pw);
+        et_pw2 = (EditText) rootView.findViewById(R.id.et_pw2);
+        et_pw_old = (EditText) rootView.findViewById(R.id.et_pw_old);
+        delsetInfo = (Button) rootView.findViewById(R.id.delsetInfo);
 
         setInfo = (Button) rootView.findViewById(R.id.setInfo);
         setInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                if (setInfo.getText().equals("设置")) {
-//                    tv_gender.setEnabled(true);
-//                    tv_nickname.setEnabled(true);
-//                    setInfo.setText("提交");
-//                } else if (setInfo.getText().equals("提交")) {
-//                    final ProgressDialog pd = ProgressDialog.show(getActivity(), getString(R.string.now_loading), getString(R.string.please_waite));
-//                    String img = "";
-//                    Bitmap bitmap = iv_image.getDrawingCache();
-//                    iv_image.setDrawingCacheEnabled(false);
-//                    if (bitmap == null) {
-//                        img = "";
-//                    } else {
-//                        img = ImageUtil.Bitmap2StrByBase64(bitmap);
-//                    }
-//
-//                    new UpdateInfo(account, token, tv_nickname.getText().toString(),
-//                            img, tv_gender.getText().toString().equals("男") ? 1 : 0,
-//                            "", "",
-//
-//                            new UpdateInfo.SuccessCallback() {
-//                                @Override
-//                                public void onSuccess(String avatag_url) {
-//                                    pd.dismiss();
-//                                    imageLoader.displayImage(Config.SERVER_URL + avatarUrl, iv_image);
-//                                    Toast.makeText(getActivity(), "更新成功!", Toast.LENGTH_LONG).show();
-//
-//                                }
-//                            },
-//                            new UpdateInfo.FailCallback() {
-//                                @Override
-//                                public void onFail() {
-//                                    onFail(Config.RESULT_STATUS_FAIL);
-//                                }
-//
-//                                @Override
-//                                public void onFail(int failCode) {
-//                                    pd.dismiss();
-//                                    switch (failCode) {
-//                                        case Config.RESULT_STATUS_FAIL:
-//                                            Toast.makeText(getActivity(), "更新失败!", Toast.LENGTH_LONG).show();
-//                                            break;
-//                                        case Config.RESULT_STATUS_INVALID_TOKEN:
-//                                            Toast.makeText(getActivity(), R.string.invalid_token_please_login_again, Toast.LENGTH_LONG).show();
-//                                            startActivity(new Intent(getActivity(), LoginActivity.class));
-//                                            break;
-//                                    }
-//                                }
-//                            });
-//                    setInfo.setText("设置");
+                if (setInfo.getText().equals("设置")) {
+                    setMyinfoVisible(View.VISIBLE);
+                    setInfo.setText("提交");
+                } else if (setInfo.getText().equals("提交")) {
+                    // 判断是否输入密码及新密码是否输入相同
+                    EditText et_pw = (EditText) rootView.findViewById(R.id.et_pw);
+                    EditText et_pw_old = (EditText) rootView.findViewById(R.id.et_pw_old);
+                    boolean isUpdatePw = false, isfalse = false;
+                    if (et_pw.getText().length() != 0 && et_pw_old.getText().length() != 0
+                            && (et_pw.getText().equals(et_pw2.getText()))) {
+                        isUpdatePw = true;
+                    } else if (et_pw.getText().length() != 0) {
+                        Toast.makeText(getActivity(), "两次密码输入不一致!", Toast.LENGTH_LONG).show();
+                        isfalse = true;
+                    }
+
+                    if (!isfalse) {
+                        final ProgressDialog pd = ProgressDialog.show(getActivity(), getString(R.string.now_loading), getString(R.string.please_waite));
+                        String img = "";
+                        Bitmap bitmap = iv_image.getDrawingCache();
+                        iv_image.setDrawingCacheEnabled(false);
+                        if (bitmap == null) {
+                            img = "";
+                        } else {
+                            img = ImageUtil.Bitmap2StrByBase64(bitmap);
+                        }
+                        new UpdateInfo(account, token, et_nickname.getText().toString(),
+                                img, et_gender.getText().toString().equals("男") ? 1 : 0,
+                                isUpdatePw ? MD5Utils.str2MD5(et_pw_old.getText().toString()) : "",
+                                isUpdatePw ? MD5Utils.str2MD5(et_pw.getText().toString()) : "",
+
+                                new UpdateInfo.SuccessCallback() {
+                                    @Override
+                                    public void onSuccess(String avatag_url) {
+                                        pd.dismiss();
+                                        imageLoader.displayImage(Config.SERVER_URL + avatarUrl, iv_image);
+                                        Toast.makeText(getActivity(), "更新成功!", Toast.LENGTH_LONG).show();
+
+                                    }
+                                },
+                                new UpdateInfo.FailCallback() {
+                                    @Override
+                                    public void onFail() {
+                                        onFail(Config.RESULT_STATUS_FAIL);
+                                    }
+
+                                    @Override
+                                    public void onFail(int failCode) {
+                                        pd.dismiss();
+                                        switch (failCode) {
+                                            case Config.RESULT_STATUS_FAIL:
+                                                Toast.makeText(getActivity(), "更新失败!", Toast.LENGTH_LONG).show();
+                                                break;
+                                            case Config.RESULT_STATUS_INVALID_TOKEN:
+                                                Toast.makeText(getActivity(), R.string.invalid_token_please_login_again, Toast.LENGTH_LONG).show();
+                                                startActivity(new Intent(getActivity(), LoginActivity.class));
+                                                break;
+                                        }
+                                    }
+                                });
+                    }
+                    //恢复为不可设置及隐藏
+                    setMyinfoVisible(View.GONE);
+                    setInfo.setText("设置");
+                }
             }
         });
+        // 选择图片
+        iv_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-//        iv_image.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                    //TODO 获得一张图片
-//                    Intent intent = new Intent();
-//                    /* 开启Pictures画面Type设定为image */
-//                    intent.setType("image/*");
-//                    /* 使用Intent.ACTION_GET_CONTENT这个Action */
-//                    intent.setAction(Intent.ACTION_GET_CONTENT);
-//                    /* 取得相片后返回MainActivity */
-//                    startActivityForResult(intent, 1);
-//
-//            }
-//        });
+                if (et_nickname.isEnabled()) {
+                    Intent intent = new Intent();
+                    /* 开启Pictures画面Type设定为image */
+                    intent.setType("image/*");
+                    /* 使用Intent.ACTION_GET_CONTENT这个Action */
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    /* 取得相片后返回MainActivity */
+                    startActivityForResult(intent, 1);
+                }
+            }
+        });
+        //
+        delsetInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //恢复为不可设置及隐藏
+                setMyinfoVisible(View.GONE);
+                setInfo.setText("设置");
+            }
+        });
 
         lazyLoad();
         return rootView;
@@ -153,7 +197,25 @@ public class MyInfoFragment extends LazyFragment {
     }
 
     private void loadMessage() {
+
     }
 
+    private void setMyinfoVisible(int view) {
+        tv_pw_new.setVisibility(view);
+        et_pw.setVisibility(view);
+        tv_pw_new2.setVisibility(view);
+        et_pw2.setVisibility(view);
+        et_pw_old.setVisibility(view);
+        tv_pw_old.setVisibility(view);
+        delsetInfo.setVisibility(view);
+
+        if (View.VISIBLE == view) {
+            et_gender.setEnabled(true);
+            et_nickname.setEnabled(true);
+        } else {
+            et_gender.setEnabled(false);
+            et_nickname.setEnabled(false);
+        }
+    }
 
 }
